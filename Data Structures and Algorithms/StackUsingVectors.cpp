ca405195a39d643db484749400b7e211797bd8c4
Ahmed <ahmedbilal2857@gmail.com>
2026-03-19 18:02:58 +0500

    Implementation of Stack Using vectors

--- Data Structures and Algorithms/StackUsingVectors.cpp
+++ Data Structures and Algorithms/StackUsingVectors.cpp
@@ -2,36 +2,92 @@
 #include <vector>
 using namespace std;
 
-class Stack{
-	vector<int> v;
+class Stack
+{
+    vector<char> v;
 public:
-	void push(int val){
-		v.push_back(val);
-	}
-	void pop(){
-		v.pop_back();
-	}
-	int top(){
-		return v[v.size()-1];
-	}
-	bool empty(){
-		if (v.size()==0)
-			return true;
-		return false;
-	}
+    void push(char val)
+    {
+        v.push_back(val);
+    }
+    char pop()
+    {
+        return top();
+    }
+    char top()
+    {
+        return v[v.size()-1];
+    }
+    bool empty()
+    {
+        if (v.size()==0)
+            return true;
+        return false;
+    }
 };
+bool isOperator(char op)
+{
+    return op == '+' || op == '-' || op == '*' || op == '/';
+}
+int precedence(char op)
+{
+    if (op == '^') return 3;
+    if (op == '*' || op == '/') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
+string infixToPostFix(string infix, Stack s)
+{
+    int i = 0;
+    string postfix = "";
+    while (i < infix.length())
+    {
+        if (isspace(infix[i]))
+        {
+            i++;
+            continue;
+        }
+        if (isdigit(infix[i]))
+        {
+            postfix += infix[i++];
+            postfix += " ";
+            continue;
+        }
+        else if (infix[i] == '(')
+        {
+            s.push(infix[i]);
+        }
+        else if (infix[i] == ')')
+        {
+            while (!s.empty() && s.top() != '(')
+            {
+                postfix += char(s.pop());
+                postfix += " ";
+            }
+            s.pop();
+        }
+        else if (isOperator(infix[i]))
+        {
+            while (!s.empty() && precedence(s.top()) >= precedence(infix[i]))
+            {
+                postfix += char(s.top());
+                postfix += " ";
+            }
+            s.push(infix[i]);
+        }
+        i++;
+    }
+    while (!s.empty()){
+		postfix += char(s.pop());
+		postfix += " ";
+    }
+    return postfix;
+}
 
-
-int main(){
-
+int main()
+{
 	Stack s;
-	s.push(10);
-	s.push(20);
-	s.push(30);
-	s.push(40);
-	while (!s.empty()){
-		cout << s.top() << endl;
-		s.pop();
-	}
-	return 0;
+	string infix = "2+3-5*6+(8/2)^2";
+
+	cout << infixToPostFix(infix, s) << endl;
 }
